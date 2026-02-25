8165585d8c3629451593f96d71f3c3ae067a18dc
FloStone <flostonedev@gmail.com>
2026-02-25 15:14:04 +0100

    Fixed bug with wrong class for style custom type

--- src/XML/Styles/StyleCustom.php
+++ src/XML/Styles/StyleCustom.php
@@ -7,6 +7,7 @@ use HearConcept\HIMSA\XML\Collection;
 use HearConcept\HIMSA\Enums\BatterySize;
 use HearConcept\HIMSA\XML\Color;
 use HearConcept\HIMSA\Enums\Side;
+use HearConcept\HIMSA\XML\ColorStyleCustom;
 
 class StyleCustom extends Style
 {
@@ -18,7 +19,7 @@ class StyleCustom extends Style
         'Side' => Side::class,
         'BuildForBatterySizeCollection' => [Collection::class, 'BatterySize', BatterySize::class],
         'ElectricalAcousticCollection' => [Collection::class, 'ElectricalAcoustic', ElectricalAcoustic::class],
-        'ColorCollection' => [Collection::class, 'ColorStyleCustom', Color::class],
+        'ColorCollection' => [Collection::class, 'ColorStyleCustom', ColorStyleCustom::class],
         'LevelInformation' => LevelInformation::class,
     ];
 
